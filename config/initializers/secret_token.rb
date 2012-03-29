370fa0b171daac1ca6d6025796fd741e02df3b0e
shivakris <shivakris@gmail.com>
2012-03-29 02:48:35 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShivaApp::Application.config.secret_token = '19e99493b3a72fa8ecc9c937daae58dae075c29baca76ab9a443d0ec7ee595363fa70972a75927638f0faa84ffd4c44ae10f49640282d1c590727455f6486272'
